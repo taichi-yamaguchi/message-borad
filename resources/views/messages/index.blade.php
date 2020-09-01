95d84bb730b2205b56fb38dca9e69f61d5b6f76a
taichi.yamagu <taizoyama44@gmail.com>
2020-09-01 13:16:50 +0000

    modifi2

--- resources/views/messages/index.blade.php
+++ resources/views/messages/index.blade.php
@@ -37,10 +37,4 @@
     
    
 
-    
-    {{-- メッセージ削除フォーム --}}
-    {!! Form::model($message, ['route' => ['messages.destroy', $message->id], 'method' => 'delete']) !!}
-        {!! Form::submit('削除', ['class' => 'btn btn-danger']) !!}
-    {!! Form::close() !!}
-
 @endsection
