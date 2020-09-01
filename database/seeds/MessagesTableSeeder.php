d3570909bd715e61487b5e6963b916fbf355c975
taichi.yamagu <taizoyama44@gmail.com>
2020-09-01 11:36:56 +0000

    Procfile

--- database/seeds/MessagesTableSeeder.php
+++ database/seeds/MessagesTableSeeder.php
@@ -11,17 +11,11 @@ class MessagesTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('messages')->insert([
-            'title' => 'test title 1',
-            'content' => 'test content 1'
-        ]);
-        DB::table('messages')->insert([
-            'title' => 'test title 2',
-            'content' => 'test content 2'
-        ]);
-        DB::table('messages')->insert([
-            'title' => 'test title 3',
-            'content' => 'test content 3'
-        ]);
+     for($i = 1; $i <= 100; $i++) {
+            DB::table('messages')->insert([
+                'title' => 'test title ' . $i,
+                'content' => 'test content ' . $i
+            ]);
     }
+}
 }
--- resources/views/messages/index.blade.php
+++ resources/views/messages/index.blade.php
@@ -27,6 +27,11 @@
         </table>
         
     @endif
+    
+    
+    {{-- ページネーションのリンク --}}
+    {{ $messages->links() }}
+    
     {{-- メッセージ作成ページへのリンク --}}
     {!! link_to_route('messages.create', '新規メッセージの投稿', [], ['class' => 'btn btn-primary']) !!}
     
